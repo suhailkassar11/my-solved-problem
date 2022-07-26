94f960291ca30ebf802b96aa4594f08a145961df
suhailkassar11 <99068201+suhailkassar11@users.noreply.github.com>
2022-07-26 12:57:56 +0530

    Time: 859 ms (20.90%), Space: 13.8 MB (53.81%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,19 +1,5 @@
 class Solution:
     def fib(self, n: int) -> int:
-        # n1=0
-        # n2=1
-        # count=1
-        # if n==0:
-        #     return 0
-        # elif n==1:
-        #     return 1
-        # else:
-        #     while count<n:
-        #         nth = n1 + n2
-        #         n1 = n2
-        #         n2 = nth
-        #         count += 1
-        # return nth
         if  n==0:
             return 0
         elif n==1:
